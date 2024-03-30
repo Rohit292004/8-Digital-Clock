b145052ecab41806b1a5af232b59915b6993cf8e
Rohit'shub <104639942+Rohit292004@users.noreply.github.com>
2024-03-30 19:46:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const time = document.getElementById('time');
+const timeformat = document.getElementById('timeformat');
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    setInterval(showTime, 1000);
+});
+
+const showTime = () =>{
+    let date = new Date();
+
+    let hrs = date.getHours();
+    let mins = date.getMinutes();
+    let secs = date.getSeconds();
+
+
+
+    hrs = hrs<10 ? `0${hrs}` : hrs;   
+    mins = mins<10 ? `0${mins}` : mins;   
+    secs = secs<10 ? `0${secs}` : secs;   
+
+
+    time.innerHTML = `${hrs} : ${mins} : ${secs}`
+
+
+    timeformat.innerHTML = hrs>12 ? "PM" : "AM"; 
+   // console.log("hours" + hrs + "mins" + mins + "secs" + secs );
+
+
+}
